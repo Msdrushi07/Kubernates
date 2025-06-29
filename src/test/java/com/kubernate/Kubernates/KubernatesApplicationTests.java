bf3b15ec06c8da8775cfdffec9c23f22777966a3
unknown <manerushikesh40@gmail.com>
2025-06-29 22:05:50 +0530

    first commit

---
+++ src/test/java/com/kubernate/Kubernates/KubernatesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kubernate.Kubernates;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KubernatesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
